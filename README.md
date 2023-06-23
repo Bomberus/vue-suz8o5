2bec6790da7c08c0ec16e643f3e883560a27080c
Bomberus <8161919+Bomberus@users.noreply.github.com>
2023-06-23 13:26:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-suz8o5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-suz8o5)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { Repl } from '@vue/repl';
+import '@vue/repl/style.css';
+</script>
+
+<template>
+  <Repl />
+</template>
